ff928740634032e2722437374975b052c95723d2
Dan Groleau <Daniel.Groleau@tivityhealth.com>
2021-06-22 16:04:08 -0600

    initial commit

---
+++ Server/Controllers/CatalogController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using PBC.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PBC.Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CatalogController : ControllerBase
+    {
+   
+
+        private readonly ILogger<CatalogController> _logger;
+
+        public CatalogController(ILogger<CatalogController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<Catalog> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new Catalog
+            {
+                id = rng.Next(1, 11)
+            }) 
+            .ToArray();
+        }
+    }
+}
---
+++ Shared/Catalog.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PBC.Shared
+{
+    public class Catalog
+    {
+        public int id { get; set; }
+    }
+}
