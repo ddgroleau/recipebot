5fcd659c97af194b3b81e752954f3cd5547e47f0
Dan Groleau <Daniel.Groleau@tivityhealth.com>
2021-07-01 16:28:04 -0600

    Initial Lazor commit

---
+++ Shared/Lazor/l.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PBC.Shared.Lazor
+{
+    public static class l
+    {
+        public static void Show()
+        {
+
+        }
+
+    }
+}
