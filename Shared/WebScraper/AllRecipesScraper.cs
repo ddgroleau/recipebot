5c873854888bb5cd24b8685eee010ed99ca38044
Dan Groleau <Daniel.Groleau@tivityhealth.com>
2021-07-09 15:50:31 -0600

    started web scraper

---
+++ Shared/WebScraper/AllRecipesScraper.cs
@@ -0,0 +1,50 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PBC.Shared.WebScraper
+{
+    public class AllRecipesScraper
+    {
+        public void Scrape(string URL)
+        {
+            HtmlWeb webPage = new HtmlWeb();
+            HtmlDocument page = webPage.Load(URL);
+
+            var title = page.DocumentNode
+                                    .SelectNodes("//h1[@class='headline heading-content']")
+                                    .FirstOrDefault().InnerHtml;
+
+            var description = page.DocumentNode
+                                    .SelectNodes("//p[@class='margin-0-auto']")
+                                    .FirstOrDefault().InnerHtml;
+
+            var ingredients = page.DocumentNode
+                                    .SelectNodes("//span[@class='ingredients-item-name']")
+                                    .Select(x => x.InnerHtml);
+
+            var instructions = page.DocumentNode
+                                    .SelectNodes("//li[@class='subcontainer instructions-section-item']")
+                                    .Descendants()
+                                    .Where(x => x.Name.Equals("p"))
+                                    .Select(x => x.InnerHtml);
+
+            var summary = page.DocumentNode
+                                    .SelectNodes("//div[@class='recipe-meta-item-body']")
+                                    .Select(x => x.InnerHtml);
+
+            var Summary = new Dictionary<string, string>()
+                {
+                    { "PrepTime",summary.ElementAt(0) },
+                    { "CookTime",summary.ElementAt(1) },
+                    { "TotalTime",summary.ElementAt(2) },
+                    { "Servings",summary.ElementAt(3) },
+                    { "Yield",summary.ElementAt(4)}
+                };
+        }
+
+    }
+}
