80b248f2477f269c6da9af6ffe634375d4c790c1
Dan Groleau <ddgroleau@gmail.com>
2021-09-14 19:38:47 -0600

    Added auth to controllers.

--- Server/Controllers/RecipeController.cs
+++ Server/Controllers/RecipeController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using PBC.Shared;
@@ -11,6 +12,7 @@ using System.Threading.Tasks;
 
 namespace PBC.Server.Controllers
 {
+    [Authorize]
     [Route("/api/[controller]")]
     [ApiController]
     public class RecipeController : ControllerBase
