a52fdb3a363348ab5b58bd609654df34be5968c9
Dan Groleau <ddgroleau@gmail.com>
2021-12-01 14:55:41 -0700

    Updated database migrations for all environments.

---
+++ Server/Data/Migrations/20211201215232_migration_20211201.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PBC.Server.Data.Migrations
+{
+    public partial class migration_20211201 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
