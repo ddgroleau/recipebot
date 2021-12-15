d1ee87d4ad0d3fe1712fbcf09bcf6d9fe33a76db
ddgroleau <ddgroleau@gmail.com>
2021-12-15 14:35:16 -0700

    Added support for SQL server

---
+++ permission.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+chmod u+x run.sh
+chmod u+x cert.sh
+chmod u+x stop.sh
+chmod u+x shell.sh
+chmod u+x rm-images.sh
+chmod u+x db-update.sh
---
+++ run.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+echo -e "Building and starting recipebot container...\n"
+docker-compose up --build -d 
